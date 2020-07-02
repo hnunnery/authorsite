795f0149d8cc4ee75282c12bbe793dc1ebf0b4d7
Hunter Nunnery <hunter.nunnery@yahoo.com>
2020-07-02 09:43:08 -0400

    Cutdowns 2

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,54 +5,30 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: "Your Business",
+  siteName: 'Your Business',
   transformers: {
     remark: {
-      externalLinksTarget: "_blank",
-      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
-      anchorClassName: "icon icon-link",
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
       plugins: [
         // ...global plugins
-      ]
-    }
+      ],
+    },
   },
 
   plugins: [
     {
-      use: "@gridsome/source-filesystem",
+      use: '@gridsome/source-filesystem',
       options: {
-        path: "blog/**/*.md",
-        typeName: "Post",
+        path: 'blog/**/*.md',
+        typeName: 'Post',
         remark: {
           plugins: [
             // ...local plugins
-          ]
-        }
-      }
+          ],
+        },
+      },
     },
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        path: "products/**/*.md",
-        typeName: "Products",
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
-        }
-      }
-    },
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        path: "home/**/*.md",
-        typeName: "Home",
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
-        }
-      }
-    }
-  ]
+  ],
 };
